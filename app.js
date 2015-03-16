627c904d4d9a4e766ae63ac3d30d996e361c7129
mariaperhaps <mariatschettino@gmail.com>
2015-03-15 22:20:29 -0400

    changed  port

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ data.makeStudentObject(function (v) {
 // console.log(students)
 })
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
 
   var host = server.address().address
   var port = server.address().port
